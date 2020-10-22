54a0d243533a7579e6452440a53fd321c422d158
Jordi Borràs i Vivó <jordibv@gmail.com>
2020-10-22 10:51:33 +0200

    Update 94_bot-live.sh

--- raspberry/94_bot-live.sh
+++ raspberry/94_bot-live.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-URL="rtmp://a.rtmp.youtube.com/live2/2wj6-5zar-93e1-bjqv-9bh9"
+URL="rtmp://a.rtmp.youtube.com/live2/xxxx"
 
-raspivid -o - -t 0 -vf -hf -fps 10 -b 500000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/2wj6-5zar-93e1-bjqv-9bh9
+raspivid -o - -t 0 -vf -hf -fps 10 -b 500000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/xxx
