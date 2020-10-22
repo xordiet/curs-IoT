3e95535124a7ca90ea5f4830cf41b90b07600df0
Jordi Borràs i Vivó <jordibv@gmail.com>
2020-10-22 10:53:42 +0200

    Update 90_bot-text.py

--- raspberry/90_bot-text.py
+++ raspberry/90_bot-text.py
@@ -1,7 +1,7 @@
 import requests
 
-bot_token = '1286093258:AAFzERZvXst-jlWpZkNYVJ-1l-ZJKMzY6Gw'
-bot_chatId = '-1001499525520'
+bot_token = 'xxxxx'
+bot_chatId = '-xxxx'
 bot_missatge = "Hola mon!"
 
 url_final = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatId + '&parse_mode=Markdown&text=' + bot_missatge
