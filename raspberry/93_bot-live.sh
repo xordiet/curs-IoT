4899361c9044f5a7a142c34f8dc8d1bf87551a0f
Jordi Borràs i Vivó <jordibv@gmail.com>
2020-10-22 10:51:04 +0200

    Update 93_bot-live.sh

--- raspberry/93_bot-live.sh
+++ raspberry/93_bot-live.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-URL="rtmp://a.rtmp.youtube.com/live2/2wj6-5zar-93e1-bjqv-9bh9"
+URL="rtmp://a.rtmp.youtube.com/live2/xxxx"
 
 raspivid -o - -t 0 -vf -hf -fps 30 -b 6000000 | ffmpeg -ac 1 -f alsa -i hw:1 -f video4linux2 -framerate 30 -i /dev/video0 -fflags nobuffer -vcodec h264_omx -g 5 -b:v 1000k -tune zerolatency -acodec aac -ab 128k -f flv URL
