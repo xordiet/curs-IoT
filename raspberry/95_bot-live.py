d74d6a85e1fd09be1e4d2839d58fde31c64100d3
Jordi Borràs i Vivó <jordibv@gmail.com>
2020-10-22 10:52:08 +0200

    Update 95_bot-live.py

--- raspberry/95_bot-live.py
+++ raspberry/95_bot-live.py
@@ -3,7 +3,7 @@ import subprocess
 import picamera 
 import time 
 YOUTUBE="rtmp://a.rtmp.youtube.com/live2/" 
-KEY="2wj6-5zar-93el-bjqv-9bh9" 
+KEY="xxxx" 
 stream_cmd = 'ffmpeg -f h264 -r 25 -i - -itsoffset 5.5 -fflags nobuffer -f alsa -ac 1 -i hw:1,0 -vcodec copy -acodec aac -ac 1 -ar 8000 -ab 32k -map 0:0 -map 1:0 -strict experimental -f flv ' + YOUTUBE + KEY 
 stream_pipe = subprocess.Popen(stream_cmd, shell=True, stdin=subprocess.PIPE) 
 camera = picamera.PiCamera(resolution=(640, 480), framerate=25) 
