1d9054b7d54f36c0cde34f9223736871c5bf0044
Jordi Borràs <jordibv@gmail.com>
2020-11-06 14:18:58 +0100

    sessió 7

---
+++ plaquesRobotiques/arduinos/practica_7.3/practica_7.3.ino
@@ -0,0 +1,32 @@
+int latchPin = 11;      // (11) ST_CP [RCK] on 74HC595
+int clockPin = 9;      // (9) SH_CP [SCK] on 74HC595
+int dataPin = 12;     // (12) DS [S1] on 74HC595
+
+//byte leds = 196; // numero entre 0 i 255 que el programa mostra el seu binari en forma de leds encesos o apagats
+int incomingByte = 0; 
+
+void updateShiftRegister(byte leds){
+   shiftOut(dataPin, clockPin, LSBFIRST, leds);
+}
+
+
+void setup() {
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);  
+  pinMode(clockPin, OUTPUT);
+  digitalWrite(latchPin, LOW);
+  Serial.begin(9600);
+}
+
+void loop() { 
+  if (Serial.available() > 0){
+      int data = Serial.parseInt();
+      Serial.println(data);
+
+      updateShiftRegister(data);
+      for (int i = 0; i < 8; i++){
+        updateShiftRegister(data);
+      }
+      digitalWrite(latchPin, HIGH);
+   }
+}
