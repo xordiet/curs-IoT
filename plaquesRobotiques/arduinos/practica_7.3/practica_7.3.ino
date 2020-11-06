932f17fd84b3ab695c8334b61be1ac9154bebd8a
Jordi Borràs i Vivó <jo@jordiborras.cat>
2020-11-06 20:27:50 +0100

    millores

--- plaquesRobotiques/arduinos/practica_7.3/practica_7.3.ino
+++ plaquesRobotiques/arduinos/practica_7.3/practica_7.3.ino
@@ -3,7 +3,6 @@ int clockPin = 9;      // (9) SH_CP [SCK] on 74HC595
 int dataPin = 12;     // (12) DS [S1] on 74HC595
 
 //byte leds = 196; // numero entre 0 i 255 que el programa mostra el seu binari en forma de leds encesos o apagats
-int incomingByte = 0; 
 
 void updateShiftRegister(byte leds){
    shiftOut(dataPin, clockPin, LSBFIRST, leds);
